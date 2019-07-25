491a3c0c4b42f128bf5210044b0293f426c7e49b
dnmacedo <dnmacedo@ucdavis.edu>
2019-07-25 16:06:34 -0700

    LBC Commit

---
+++ lbc/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+  // LBC WEBSITE
+    var newWeb = document.getElementById("new-website");
+    var oldWeb = document.getElementById("old-website");
+    var webPic = document.getElementById("website-pic");
+  // LBC LOGO
+    var newLogo = document.getElementById("new-logo");
+    var oldLogo = document.getElementById("old-logo");
+    var logoPic = document.getElementById("logo-pic");
+
+    newWeb.addEventListener("click", function(){
+      webPic.src="img/Website.jpg";
+    });
+    oldWeb.addEventListener("click", function(){
+      webPic.src="img/Old_Website.png";
+    });
+    newLogo.addEventListener("click", function(){
+      logoPic.src = "img/LeathermanLogo2.png";
+      logoPic.style = "margin: 20px 100px 50px 100px;";
+    });
+    oldLogo.addEventListener("click", function(){
+      logoPic.src = "img/Current-Logo.jpg";
+      logoPic.style = "margin:100px 100px 50px 100px;";
+    });
+  });
