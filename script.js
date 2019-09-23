21a0ab6c9a22134a9f0df855d970f00b48264500
dnmacedo <dnmacedo1@gmail.com>
2019-09-23 16:52:30 -0700

    debugged pop up nav function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event){
   console.log("DOM fully loaded and parsed");
 
 var menu = document.getElementById('menu');
-var nav = document.getElementsByTagName('nav');
+var nav = document.getElementById('mobile-nav');
 var close = document.getElementById('close');
 
 var github = document.getElementById("github-icon");
@@ -10,19 +10,23 @@ var ig = document.getElementById("ig-icon");
 var linkedin = document.getElementById("linkedin-icon");
 
 menu.addEventListener('click', function(){
-  nav.style.display='block';
-  console.log("clicked");
+  nav.style.display = "block";
   // add a fade up?
 });
 
 close.addEventListener('click', function(){
-  nav.style.display='none';
+  nav.style.display = "none";
 });
 
 if (screen.width < 480){
   github.src = "img/github-icon-white.png";
   ig.src = "img/ig-icon-white.png";
   linkedin.src = "img/linkedin-icon-white.png";
+}
+else {
+  github.src = "img/github-icon.svg";
+  ig.src = "img/ig-icon.png";
+  linkedin.src = "img/linkedin-icon.png";
 }
 });
 // add a cursor hover
