1d8db40f425b870908658f57ebee0b688c48e2c3
dnmacedo <dnmacedo1@gmail.com>
2019-10-21 17:18:38 -0700

    created new inner-page JS file, for accurate file routing

---
+++ script-innerPages.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+
+var menu = document.getElementById('menu');
+var nav = document.getElementsByTagName('nav')[0];
+var close = document.getElementById('close');
+
+var github = document.getElementById("github-icon");
+var ig = document.getElementById("ig-icon");
+var linkedin = document.getElementById("linkedin-icon");
+
+menu.addEventListener('click', function(){
+  console.log("clicked");
+  nav.style.display = "block";
+  // add a fade up?
+});
+
+close.addEventListener('click', function(){
+  nav.style.display = "none";
+});
+
+if (screen.width < 768){
+  github.src = "../img/github-icon-white.png";
+  ig.src = "../img/ig-icon-white.png";
+  linkedin.src = "../img/linkedin-icon-white.png";
+}
+else {
+  github.src = "../img/github-icon.svg";
+  ig.src = "../img/ig-icon.png";
+  linkedin.src = "../img/linkedin-icon.png";
+}
+});
+// add a cursor hover
